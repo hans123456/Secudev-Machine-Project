a5023347a7ad90a5d80685b631550bc0e2c9b61e
hans123456 <h.g.n.chua@gmail.com>
2015-10-28 19:02:53 +0800

    fix paypalaccess

--- src/utilities/PayPalAccess.java
+++ src/utilities/PayPalAccess.java
@@ -12,21 +12,30 @@ public class PayPalAccess {
 	private static String ClientID = "AVy6zrSTVCeJihOa0tL_8Eyvz6l1HDwANrShMtZ-GG2sSyBoUyurPdzy8MuIetVM-0dJwHiH47FXSV1k";
 	private static String Secret = "EKR3ps6au38cDd0eRXlaOfz-Eomo7D1fS_0ZWhpAyyPyUQYJuADczOS__LoIxidpLZ9vHm-ss_to2xOx";
 
+	private static OAuthTokenCredential tokenCredential;
 	private static String accessToken;
+	private static long timeUpdated;
+	private static long expiresIn;
 
 	private static Map<String, String> sdkConfig;
 
 	static {
 		sdkConfig = new HashMap<String, String>();
 		sdkConfig.put(Constants.MODE, Constants.SANDBOX);
-		try {
-			accessToken = new OAuthTokenCredential(ClientID, Secret, sdkConfig).getAccessToken();
-		} catch (PayPalRESTException e) {
-			e.printStackTrace();
-		}
+		timeUpdated = expiresIn = 0;
 	}
 
 	public static String getAccessToken() {
+		long currentTime = System.currentTimeMillis();
+		if (currentTime - timeUpdated > expiresIn) {
+			try {
+				tokenCredential = new OAuthTokenCredential(ClientID, Secret, sdkConfig);
+				accessToken = tokenCredential.getAccessToken();
+				expiresIn = tokenCredential.expiresIn() * 900;
+			} catch (PayPalRESTException e) {
+				e.printStackTrace();
+			}
+		}
 		return accessToken;
 	}
 
